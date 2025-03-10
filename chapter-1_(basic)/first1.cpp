8b1249caef2fd04919356cb7813fcbf06b17b257
Sachinkr <royalsachin1530@gmail.com>
2025-03-11 01:07:43 +0530

    Add files via upload

---
+++ chapter-1_(basic)/first1.cpp
@@ -0,0 +1,23 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+
+//     int a,b,c;
+    
+//     cout<<"Enter 2 NO ";
+//     cin>>a>>b;
+//     c=a+b;
+//     cout<<"Add "<<c<<endl;
+//     return 0;
+    
+// }
+
+#include<iostream>
+using namespace std;
+int main(){
+    string str;
+    cout<<"May i know your name ";
+    getline(cin,str);
+    cout<<"Welcome mrs "<<str<<endl;
+    return 0;
+}
