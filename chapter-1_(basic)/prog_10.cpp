3dc8ecd40b52e5c110f1078ff82bdd0d141e37a6
Sachinkr <royalsachin1530@gmail.com>
2025-03-23 00:47:27 +0530

    Add files via upload

---
+++ chapter-1_(basic)/prog_10.cpp
@@ -0,0 +1,19 @@
+//  Bitwise operators
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int x=11,y=7,z;
+    // z=x&y;
+    // cout<<z<<endl;
+
+    // z=x|y;
+    // cout<<z<<endl; 
+
+    // z=x^y;
+    // cout<<z<<endl;
+    char x=20,y;
+    y=x<<1;
+    cout<<(int)y<<endl;
+}
