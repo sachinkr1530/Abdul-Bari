6147cfe6baefbba1051c5d691cb65b10f26e6acc
Sachinkr <royalsachin1530@gmail.com>
2025-03-23 11:52:04 +0530

    Add files via upload

---
+++ chapter-1_(basic)/prog_11.cpp
@@ -0,0 +1,25 @@
+//  Enum program 
+
+// #include <iostream>
+// using namespace std;
+// enum day {mon,tue,wed,thu,fri,sat,sun};
+
+// int main()
+// {
+//     day d;
+//     d=tue;
+//     cout<<d<<endl;
+// }
+
+
+
+#include<iostream>
+using namespace std;
+typedef int marks;
+int main()
+{
+    marks m1,m2;
+    m1=50;
+    m2=90;
+    cout<<m1<<" "<<m2<<endl;
+}
