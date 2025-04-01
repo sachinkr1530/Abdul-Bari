b4254e6f2778a073b9d9e366d338f1e8c48b85f5
Sachinkr <royalsachin1530@gmail.com>
2025-04-01 22:19:42 +0530

    Add files via upload

---
+++ chapter-3_(Loops)/prog_1_loop.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+ using namespace std;
+ int main()
+{
+  int i=0;
+  while(i<10)
+  {
+    cout<<"Hello";
+    i++;
+  }
+} 
+
+
+
+
+
+
+//  {
+//     int a=0;
+//     while(a<10)
+//     {
+//      cout<<"a "<<a;
+//      a++;
+//     }
+//     int b=0;
+//     do
+//     {
+//       cout<<"b "<<b;
+//       b++;
+//     }
+//     while(b<10);
+//      for(int i=0;i<10;i++)
+//     {
+//         cout<<"i "<<i;
+//     }
+//      return 0;
+//  }
