5b33d65405680d298b04b5c8f29c95763885791b
Sachinkr <royalsachin1530@gmail.com>
2025-04-07 23:55:21 +0530

    Add files via upload

---
+++ chapter-4_(Arrays)/prog_2_adding_all_elem.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[7]={4,8,6,9,5,2,7};
+    int n=7,sum=0;
+    for(int i=0;i<7;i++)
+    {
+        sum=sum+a[i];
+    }
+    cout<<"Sum of all no is: "<< sum <<endl;
+    return 0;
+}
